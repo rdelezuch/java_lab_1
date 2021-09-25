21d3a035441f35343ec244c68be9103422ba1229
rdelezuch <robert.delezuch@gmail.com>
2021-09-25 10:31:20 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true, b = true, c = false;
+        int x = 2, y = 4, z = -3;
+
+        System.out.println("("+a+"&&"+c+") || ("+!b+"&&"+a+") = " + ((a && c) || (!b && a)));
+        System.out.println("((("+x+"+"+y+")>"+z+") || ("+y+"=="+z+")) && ("+x+"!="+z+") = " + ((((x+y)>z) || (y==z)) && (x != z)));
+
     }
 }
