5bbb85d32e1896cb5b8af0a2be6533a0ee50061e
rdelezuch <robert.delezuch@gmail.com>
2021-09-25 10:52:57 +0200

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,12 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        int[] tab = {34, 54, 23, 1, 100};
+
+        for (int i = 0; i < tab.length; i++)
+        {
+            System.out.println("Liczba "+(i+1)+ ": " + tab[i]);
+        }
+
     }
 }
