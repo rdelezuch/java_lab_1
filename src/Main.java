a73d7e68bc3580a4511ce24d9b2394f6a378edfc
rdelezuch <robert.delezuch@gmail.com>
2021-09-25 09:04:40 +0200

    ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Dzień Dobry, to zwykły tekst, który robi od razu enter");
+        System.out.printf("Teraz %s z %s i przy okazji użycie %s do zrobienia entera\n","przykład","interpolacją", "/n");
+        System.out.print("A tutaj zwykły tekst. ");
+        System.out.print("I drugi zwykły tekst");
 
     }
 }
