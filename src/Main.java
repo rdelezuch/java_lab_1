ce4e7a643ec9b780ccdbd09a92ec44586c8abc40
rdelezuch <robert.delezuch@gmail.com>
2021-09-25 10:09:34 +0200

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,46 @@ public class Main {
         *   b) wyświetlić dane za pomocą println i printf
         */
 
+        System.out.println("zadanie 1\n");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj imię: ");
+        String imie = scan.next();
+
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("Podaj nr indeksu: ");
+        String nrIndeksu = scan.next();
+
+        System.out.print("Podaj wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.println("\nWprowadzone dane to: \n");
+        System.out.println("Imię: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.printf("Nr indeksu: %s\n", nrIndeksu);
+        System.out.printf("Wiek: %d\n", wiek);
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+
+        System.out.println("\n zadanie 2\n");
+
+        System.out.print("Podaj pierwszą liczbę: ");
+        int a = scan.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        int b = scan.nextInt();
+
+        System.out.println("\nWyniki działań na tych 2 liczbach\n");
+
+        System.out.printf("%d + %d = %d\n", a, b, a+b);
+        System.out.printf("%d - %d = %d\n", a, b, a-b);
+        System.out.printf("%d * %d = %d\n", a, b, a*b);
+        System.out.printf("%d / %d = %d\n", a, b, a/b);
+        System.out.printf("%d %s %d = %d\n", a,"%", b, a%b);
 
     }
 }
